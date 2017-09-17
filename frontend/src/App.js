1a9f0723f6a151147c853edc132e13ffd3fbe007
joshua <joshkarges@gmail.com>
2017-09-17 19:32:35 -0400

    all actions, and the reducers can handle the get actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-console.log('Process env', process.env);
 
 class App extends Component {
   constructor(props) {
@@ -26,13 +23,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <p>
           Talking to the backend yields these categories: <br/>
           {this.state.backend}
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,11 +1,42 @@
-// import { } from '../actions'
+import {
+  GET_ALL_CATEGORIES,
+  GET_ALL_POSTS,
+  GET_POSTS_BY_CATEGORY,
+  GET_POST_BY_ID,
+  GET_COMMENTS_BY_POST,
+  GET_COMMENT_BY_ID
+} from '../actions'
 import { combineReducers } from 'redux';
 
-function reducer(state={}, action) {
+function categories(state={}, action) {
   switch (action.type) {
+    case GET_ALL_CATEGORIES:
     default:
       return state;
   }
 }
 
-export default combineReducers({ reducer })
+function posts(state={}, action) {
+  switch (action.type) {
+    case GET_ALL_POSTS:
+    case GET_POSTS_BY_CATEGORY:
+    case GET_POST_BY_ID:
+    default:
+      return state;
+  }
+}
+
+function comments(state={}, action) {
+  switch (action.type) {
+    case GET_COMMENTS_BY_POST:
+    case GET_COMMENT_BY_ID:
+    default:
+      return state;
+  }
+}
+
+export default combineReducers({
+  categories,
+  posts,
+  comments
+})
