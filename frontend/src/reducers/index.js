d816e256492baec83419c65fbd1ea00686671552
joshua <joshkarges@gmail.com>
2017-09-17 01:30:09 -0400

    react redux skeleton

---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,11 @@
+// import { } from '../actions'
+import { combineReducers } from 'redux';
+
+function reducer(state={}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default combineReducers({ reducer })
