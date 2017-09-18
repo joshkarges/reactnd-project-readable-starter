875352112c4756690e64370e302ead3418ac6858
joshua <joshkarges@gmail.com>
2017-09-18 09:47:39 -0400

    redux extension enhancer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers';
 
 const store = createStore(
   reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   // composeEnhancers(
   //   applyMiddleware(logger)
   // )
 );
+
+console.log(store.getState());
 
 ReactDOM.render(<Provider store={store}>
     <App />
